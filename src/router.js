c1c966c3c3213f655bce5b32a5bf46c7ff5f8303
vefaozlu <vefaozlu01@gmail.com>
2024-03-25 21:24:06 +0300

    Start route added

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Controller from "./controller.js";
 
 const router = Router();
 
+router.route("/start").get(Controller.start);
+
 router.route("/refresh").post(Controller.refresh);
 
 router.route("/token").post(Controller.token);
