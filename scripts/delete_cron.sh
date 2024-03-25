05201bbba4f2298e470bdfedd1a28c57d957e51d
vefaozlu <vefaozlu01@gmail.com>
2024-03-25 21:52:45 +0300

    gitignore changes

--- scripts/delete_cron.sh
+++
@@ -1,3 +0,0 @@
-echo "" >| emptycron
-crontab emptycron
-rm emptycron
--- scripts/new_cron.sh
+++
@@ -1,9 +0,0 @@
-echo "" >| emptycron
-crontab emptycron
-rm emptycron
-
-crontab -l > mastercron
-echo "* * * * * /home/ubuntu/Projects/Test/refresh_script.sh" > mastercron
-
-crontab mastercron
-rm mastercron
